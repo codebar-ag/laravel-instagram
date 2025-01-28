5a08114a0620c90c980f10197e000933e64186b2
RhysLees <43909932+RhysLees@users.noreply.github.com>
2025-01-28 12:44:02 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,8 @@
 <?php
 
 use CodebarAg\LaravelInstagram\Tests\TestCase;
-use Illuminate\Support\Sleep;
 use Saloon\Config;
 use Saloon\Http\Faking\MockClient;
-use Saloon\Laravel\Facades\Saloon;
 
 Config::preventStrayRequests();
 
